8303858242947c644a09347de5ee85bd1773590c
BH <bh.kim@nemolab.co.kr>
2021-01-18 19:34:23 +0900

    Create packages folder which has site and standard packages

---
+++ packages/site_packages.py
@@ -0,0 +1,33 @@
+
+import pandas
+from notion.client import NotionClient
+from PIL import Image
+import requests
+from bs4 import BeautifulSoup
+import pyexcel.cookbook as pc #엑셀파일 제어
+import pyexcel as px
+import requests #웹 요청을 통해 응답을 받아옴
+from bs4 import BeautifulSoup #BeutifulSoup (CSS 제어)
+import matplotlib.pyplot as plt 
+import cairosvg
+import numpy as np 
+import datetime
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw #https://pillow.readthedocs.io/en/stable/
+import svgwrite #https://svgwrite.readthedocs.io/en/latest/
+from selenium import webdriver
+
+import matplotlib #데이터 시각화
+import seaborn #matplotlib 기반 시각화 라이브러리
+import sklearn #머신러닝 라이브러리 (데이터 가공, 모델평가)
+import tensorflow #자연어처리에 많이 사용되는 RNN 등을 구현
+import pytorch
+import keras
+import nltk #텍스트 데이터 가공, 시각화 
+
+import django #웹 프레임워크
+import flask #웹 프레임워크
+import opencv #이미지 프로세싱, 얼굴 인식, 문자 인식 등
+import youtube_dl #유튜브 영상 다운로드
+
---
+++ packages/standard_module.py
@@ -0,0 +1,16 @@
+# docs.python.org
+
+import math
+import random
+import datetime
+import os
+# from os import path
+import json
+import pickle
+import copy
+import sqlite3
+import re
+import sys
+
+
+print(sys.path)
